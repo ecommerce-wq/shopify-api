3e21d34fe6a0df1acb97798f62d1c91d68ad50eb
ecommerce-wq <ecommerce@houseofsartorial.com>
2026-04-11 13:55:13 -0500

    Update sync-products.js

--- api/sync-products.js
+++ api/sync-products.js
@@ -1,9 +1,31 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch(
-      "https://srv2.best-fashion.net/ApiV3/token/38712c15e4976ba5f4647e891f559271/callType/allStockGroup"
-    );
+   const shopifyResponse = await fetch(
+  "https://houseofsartorial.myshopify.com/admin/api/2023-10/products.json",
+  {
+    method: "POST",
+    headers: {
+      "X-Shopify-Access-Token": "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",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      product: {
+        title: item.name,
+        body_html: item.description,
+        vendor: item.brand,
+        product_type: item.category,
+        variants: [
+          {
+            price: item.price,
+          },
+        ],
+      },
+    }),
+  }
+);
 
+const result = await shopifyResponse.json();
+console.log(result);
     const data = await response.json();
 
     if (!data) {
