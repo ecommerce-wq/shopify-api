05e3319254472a5402aed1492e52a4883e1145cc
ecommerce-wq <ecommerce@houseofsartorial.com>
2026-04-11 20:47:33 -0500

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,21 +1,29 @@
 // test deploy 
 
 export default function handler(req, res) {
-  const shop = process.env.xkhkiu-up.myshopify.com;
-  const client_id = process.env.e7a2b1dc9172957559f3c094c2191024;
+  try {
+    const shop = process.env.xkhkiu-up.myshopify.com;
+    const client_id = process.env.e7a2b1dc9172957559f3c094c2191024;
 
-  if (!shop || !client_id) {
+    if (!shop || !client_id) {
+      return res.status(500).json({
+        error: "Variables faltantes",
+        shop,
+        client_id
+      });
+    }
+
+    const redirect_uri = `https://shopify-api-tlow.vercel.app/api/auth/callback`;
+    const scope = "read_products,write_products";
+
+    const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}`;
+
+    return res.redirect(installUrl);
+
+  } catch (error) {
     return res.status(500).json({
-      error: "Faltan variables de entorno",
-      shop,
-      client_id
+      error: "Crash en auth",
+      detalle: error.message
     });
   }
-
-  const redirect_uri = `https://${req.headers.host}/api/auth/callback`;
-  const scope = "read_products,write_products";
-
-  const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}`;
-
-  res.redirect(installUrl);
 }
