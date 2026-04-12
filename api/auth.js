466060b79313e858612f9f468b017dc9e70dfedb
ecommerce-wq <ecommerce@houseofsartorial.com>
2026-04-11 21:23:03 -0500

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   try {
-    const shop = process.env.houseofsartorial.com;
+    const shop = process.env.xkhkiu-up.myshopify.com;
     const client_id = process.env.e7a2b1dc9172957559f3c094c2191024;
 
     if (!shop || !client_id) {
