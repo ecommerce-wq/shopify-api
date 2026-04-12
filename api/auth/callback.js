6d088a0fa2ec6d9759bc98f5ab5465a371300f8f
ecommerce-wq <ecommerce@houseofsartorial.com>
2026-04-11 19:56:57 -0500

    Delete api/auth directory

--- api/auth/callback.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { shop, code } = req.query;
-
-  res.status(200).json({
-    message: "Callback funcionando",
-    shop,
-    code
-  });
-}
