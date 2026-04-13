2b1548f3e95871742c26a74b82b563dd3198bb00
ecommerce-wq <ecommerce@houseofsartorial.com>
2026-04-13 15:11:27 -0500

    Update sync-products.js

--- api/sync-products.js
+++ api/sync-products.js
@@ -3,33 +3,14 @@ export default async function handler(req, res) {
     const shop = process.env.SHOPIFY_SHOP;
     const token = process.env.SHOPIFY_ACCESS_TOKEN;
 
-    if (!shop || !token) {
-      return res.json({
-        error: "Variables no definidas",
-        shop,
-        token
-      });
-    }
-
-    const response = await fetch(
-      `https://${shop}/admin/api/2024-04/products.json`,
-      {
-        headers: {
-          "X-Shopify-Access-Token": token
-        }
-      }
-    );
-
-    const text = await response.text();
-
     return res.json({
-      status: response.status,
-      debug: text
+      shop,
+      token
     });
 
   } catch (error) {
     return res.status(500).json({
-      error: "Crash real",
+      error: "Crash total",
       detalle: error.message
     });
   }
