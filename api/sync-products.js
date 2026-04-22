c7ffae753f9b1b034b96380c926b466ec017e660
ecommerce-wq <ecommerce@houseofsartorial.com>
2026-04-22 15:02:48 -0500

    Update sync-products.js

--- api/sync-products.js
+++ api/sync-products.js
@@ -63,9 +63,6 @@ export default async function handler(req, res) {
 
    const proveedorData = await proveedorResponse.json();
 
-return res.json({
-  debug: proveedorData
-});
 
     // 🔹 BASE DE IMÁGENES
     const imageBaseResponse = await fetch(
